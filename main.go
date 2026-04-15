cbcfc36de2afab14d96bebc1fcc1dda526bd86fd
Gpontes143 <gpontes143@gmail.com>
2026-04-15 16:26:09 -0300

    feature: compile executando
    falta testes com uma placa para confirmar se esta funcionando

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"lazyarduino/pkg/commands"
 )
 
-func board() {
+func board() string {
 	fmt.Println("Buscando placas conectadas...")
 
 	boards, err := commands.ListBoards()
@@ -17,18 +17,35 @@ func board() {
 
 	if len(boards) == 0 {
 		fmt.Println("Nenhuma placa encontrada.")
+		return ""
+	}
+	b := boards[0]
+	fqbn := ""
+	nome := "desconhecido"
+
+	if len(b.MatchingBoards) > 0 {
+		nome = b.MatchingBoards[0].Name
+		fqbn = b.MatchingBoards[0].FQBN
+	}
+	fmt.Printf("- Porta: %s | Placa: %s\n", b.Port.Address, nome)
+	return fqbn
+}
+
+func compile(path string, fqbn string) {
+	fmt.Printf("Compilando...")
+	out, err := commands.Compile(fqbn, path)
+	if err != nil {
+		fmt.Printf("Erro de compilação: \n%s", out)
 		return
 	}
-
-	for _, b := range boards {
-		nome := "Desconhecido"
-		if len(b.MatchingBoards) > 0 {
-			nome = b.MatchingBoards[0].Name
-		}
-		fmt.Printf("- Porta: %s | Placa: %s\n", b.Port, nome)
-	}
+	fmt.Printf("Compilado com sucesso")
 }
 
 func main() {
-	board()
+	fqbn := board()
+	path := "."
+	if fqbn == "" {
+		fmt.Println("Não foi possivel identificar o fqbn automaticamente!")
+	}
+	compile(fqbn, path)
 }
